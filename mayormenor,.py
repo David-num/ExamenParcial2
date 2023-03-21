8d1771636097307419e7fa588ef90b6acc2feed6
David-num <teretasosof@gmail.com>
2023-03-21 09:54:10 -0600

    Squashed commit of the following:
    commit 6cc0d70b274b4972e90ac697dc15eaa8e6c1aef2
    Author: David-num <teretasosof@gmail.com>
    Date:   Tue Mar 21 09:52:26 2023 -0600
    
        Squashed commit of the following:
    
        commit 67b23e7a8933fc699addf4978acf84f648480f64
        Author: David-num <teretasosof@gmail.com>
        Date:   Tue Mar 21 09:41:10 2023 -0600
    
            Programa
    
            Se creo un programa para ingresar dos números y verificar cual es mayor.
    
    commit 178a9505ea2b34c5441146fd8fdf45dce799d2f7
    Author: David-num <teretasosof@gmail.com>
    Date:   Tue Mar 21 09:51:47 2023 -0600
    
        Squashed commit of the following:
    
        commit 9ff6b28235cd82ff1e3d173c88ab3076e5b8f42d
        Author: David-num <teretasosof@gmail.com>
        Date:   Tue Mar 21 09:46:33 2023 -0600
    
            programa
    
            Se agrego un programa para verificar si es par o no
    
    commit 683dc1ee1b95fc4b71b5250724ca46eba402093b
    Author: David-num <teretasosof@gmail.com>
    Date:   Tue Mar 21 09:34:28 2023 -0600
    
        Nombre
    
        Se agrego el nombre y el carnet

---
+++ mayormenor,.py
@@ -0,0 +1,8 @@
+x=int(input('Ingrese el primer numero'))
+x2=int(input('Ingrese el segundo numero numero'))
+if x > x2:
+    print('El mayor es', x)
+elif x2 > x:
+    print('El mayor es', x2)
+else :
+    print('Los dos son iguales')
